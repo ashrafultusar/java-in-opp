c5c7f3ee10ebd08e1c4acfbd7c1e934be811f247
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-10-07 16:22:59 +0600

    update pen type

--- OOPS.java
+++ OOPS.java
@@ -16,6 +16,6 @@ public class OOPS{
     public static void main(String[] args) {
         Pen pen1 = new Pen();
         pen1.color = "blue";
-        pen1.type = "gel";
+        pen1.type = "gelpen";
     }
 }
