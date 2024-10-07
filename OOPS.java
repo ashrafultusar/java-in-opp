ceae900f4ca0bdfd645b33ad0094003c033a163a
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-10-07 18:45:34 +0600

    added update java class and objects code

--- OOPS.java
+++ OOPS.java
@@ -1,21 +1,50 @@
+// pen class 
 class Pen{
     String color;
     String type;
 
+    // Methods
     public void write() {
         System.out.println("writing something");
     }
 
     public void printColor() {
         System.out.println(this.color);
+        System.out.println(this.type);
     }
+
+}
+
+// student class
+ class Student {
+     String name;
+     int age;
+     int intake;
+     
+     public void printInfo() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+        System.out.println(this.intake);
+     }
+    
 }
 
 
 public class OOPS{
-    public static void main(String[] args) {
+    public static void main(String args[]) {
+        // Object creation (pen1 and pen2 are instances of the Pen class)
         Pen pen1 = new Pen();
         pen1.color = "blue";
-        pen1.type = "gelpen";
+        pen1.type = "gel-pen";
+
+        Pen pen2 = new Pen();
+        pen2.color="Black";
+        pen2.type = "ballpoint";
+
+        // Calling methods on objects
+        pen1.printColor();
+        pen2.printColor();
     }
 }
+
+
